93482c4e6521ae2e7958f8be3a3c85c12ea87afe
srujana <srujana@gmail.com>
2025-07-28 09:34:18 +0530

    backend-server

--- hireSync_server/server.js
+++ hireSync_server/server.js
@@ -21,6 +21,6 @@ app.use("/api/jobs", jobRoutes);
 app.use("/api/applications", require("./routes/applicationRoutes"));
 app.use("/uploads", uploadRoutes);
 
-app.listen(5000, () => {
-  console.log("✅ Backend running on http://localhost:5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
